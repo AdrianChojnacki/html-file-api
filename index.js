065384a870865bd0839377b8440c9bb5974cec27
Adrian Chojnacki (PGS Software) <achojnacki@pgs-soft.com>
2022-06-17 11:30:51 +0200

    added alert

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ const inpFile = document.getElementById("inpFile");
 
 inpFile.addEventListener("change", function () {
   console.log(inpFile.files);
+
+  for (const file of inpFile.files) {
+    if (file.size > 300000) {
+      alert(`${file.name} is too big! Max is 300KB`);
+    }
+  }
 });
